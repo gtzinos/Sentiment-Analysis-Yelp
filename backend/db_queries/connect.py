fc91f2518f20f3c43dd80c839fd5f7aad6fcbf0e
George Tzinos <geotzinos@gmail.com>
2018-05-08 01:14:33 +0300

    ADD: Close connection

--- backend/db_queries/connect.py
+++ backend/db_queries/connect.py
@@ -3,3 +3,6 @@ import pymongo
 def openConnection(hostname, dbname, port):
     connection = pymongo.MongoClient(hostname, port)
     return connection[dbname]
+
+def closeConnection(connection):
+    connection.close()
