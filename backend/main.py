6f97eca548b51ef2e20c9b32537ad79483168d23
George Tzinos <geotzinos@gmail.com>
2018-05-10 13:57:41 +0300

    FIX: Main steps

--- backend/main.py
+++ backend/main.py
@@ -5,16 +5,38 @@ from data_analysis import *
 
 db = openConnection(db_hostname, db_name, db_port)
 
-def relation_stars_with_textlength():
+def preprocessing(dataList, fieldNames):
+    for fieldName in fieldNames:
+        print(fieldName)
+        for row in dataList:
+            print(row)
+
+            row[fieldName] = tokenization(row[fieldName])
+            row[fieldName] = remove_non_ascii(row[fieldName])
+            row[fieldName] = to_lowercase(row[fieldName])
+            row[fieldName] = remove_punctuation(row[fieldName])
+            row[fieldName] = replace_numbers(row[fieldName])
+            row[fieldName] = remove_stopwords(row[fieldName])
+
+    return dataList
+
+def main():
     global db
 
-    rere = Reviews(name = db_reviews_table_name)
+    reviews = Reviews(name = db_reviews_table_name)
 
-    data = rere.toDataFrame(rere.find_all(db))
+    data = reviews.get_filtered_reviews(db,1, 10)
 
-    seaborn(data)
+    print(data)
 
-relation_stars_with_textlength()
+    data = preprocessing(data, ["text"])
+
+    print(data)
+    #seaborn(data)
+
+
 #top = find_count(db, "reviews")
 
 #print(top)
+if __name__ == "__main__":
+    main()
