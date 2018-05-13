58df69a01ad56cdcf0fca0ef01ee5575d4502fe8
George Tzinos <geotzinos@gmail.com>
2018-05-13 23:54:56 +0300

    ADD Lexicons

--- src/app/components/lexicons/lexicons.module.ts
+++ src/app/components/lexicons/lexicons.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LexiconsComponent } from './lexicons.component';
 import { RouterModule } from '@angular/router';
-import {MatCardModule} from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatInputModule, MatTooltipModule, MatToolbarModule } from '@angular/material';
+import { OverlayModule } from '@angular/cdk/overlay';
 
 @NgModule({
   imports: [
-    CommonModule,MatIconModule,MatCardModule, FormsModule, MatButtonModule,
-    RouterModule.forChild([{path: '', component: LexiconsComponent}])
+    CommonModule, MatIconModule,OverlayModule, MatToolbarModule, MatSortModule, MatInputModule, MatTooltipModule, MatPaginatorModule, MatTableModule, FormsModule, MatButtonModule,
+    RouterModule.forChild([{ path: '', component: LexiconsComponent }])
   ],
   declarations: [LexiconsComponent]
 })
