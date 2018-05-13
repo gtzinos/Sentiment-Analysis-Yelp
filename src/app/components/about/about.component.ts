50b3047deffd38d4d1f8d36147343ad81f641a61
George Tzinos <geotzinos@gmail.com>
2018-05-13 18:28:33 +0300

    ADD: About page

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -7,6 +7,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
+
+  developers = [
+    {
+      name: "Ilias Karatsin",
+      title: "Full Stack Developer",
+      photo: "https://scontent.fskg1-1.fna.fbcdn.net/v/t1.0-9/13226931_1233808183350275_6020287782455920427_n.jpg?_nc_cat=0&oh=78d74adeab14cb24b4af5997c6e7d9d6&oe=5B81C380",
+      description: "Hello...",
+      profiles: [
+        {
+          id: "git",
+          url: ""
+        }
+      ]
+    },
+    {
+      name: "Marinos Zagotsis",
+      title: "Full Stack Developer",
+      photo: "https://scontent.fskg1-1.fna.fbcdn.net/v/t31.0-8/13048111_10209441039527662_864103833430926887_o.jpg?_nc_cat=0&oh=c5894f0645922afbfd47479fff410785&oe=5B93BC74",
+      description: "Hello...",
+      profiles: [
+        {
+          id: "git",
+          url: ""
+        }
+      ]
+    },
+    {
+      name: "Ilias Karatsin",
+      title: "Full Stack Developer",
+      photo: "https://scontent.fskg1-1.fna.fbcdn.net/v/t1.0-9/13165870_10206152945047169_2926124721482332118_n.jpg?_nc_cat=0&oh=5fc8d7391a79a5699943be0ef1ee3812&oe=5B8C8D43",
+      description: "Hello...",
+      profiles: [
+        {
+          id: "git",
+          url: ""
+        }
+      ]
+    },
+    {
+      name: "George Tzinos",
+      title: "Full Stack Developer",
+      photo: "/assets/tzinos.png",
+      description: "Hello...",
+      profiles: [
+        {
+          id: "git",
+          url: ""
+        }
+      ]
+    }
+  ]
   constructor() { }
 
   ngOnInit() {
--- src/app/components/about/about.module.ts
+++ src/app/components/about/about.module.ts
@@ -1,11 +1,14 @@
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AboutComponent } from './about.component';
 import { RouterModule } from '@angular/router';
+import {MatCardModule} from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   imports: [
-    CommonModule,
+    CommonModule,MatCardModule, FormsModule, MatButtonModule,
     RouterModule.forChild([{path: '', component: AboutComponent}])
   ],
   declarations: [AboutComponent]
