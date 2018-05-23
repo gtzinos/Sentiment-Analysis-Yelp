5f8f1a987f7ae4e55de4f1607975be411e3ffb45
George Tzinos <geotzinos@gmail.com>
2018-05-24 00:10:16 +0300

    ADD Lexicons

--- src/app/components/lexicons/lexicons.component.ts
+++ src/app/components/lexicons/lexicons.component.ts
@@ -1,10 +1,52 @@
-import { Component} from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-lexicons',
   templateUrl: './lexicons.component.html',
   styleUrls: ['./lexicons.component.css']
 })
-export class LexiconsComponent {
+export class LexiconsComponent implements AfterViewInit {
+
+  @ViewChild("myChart") mychart: ElementRef<any>;
+
+  public chartObj;
+  public chartContext;
+  public chartBody;
+
+  // Somewhere under the class constructor we want to wait for our view
+  // to initialize
+
+  ngAfterViewInit(): void {
+    let chartElement = this.mychart.nativeElement.getContext('2d');
+
+    var data = {
+      labels: [
+        "Value A",
+        "Value B"
+      ],
+      datasets: [
+        {
+          "data": [101342, 55342],   // Example data
+          "backgroundColor": [
+            "#1fc8f8",
+            "#76a346"
+          ]
+        }]
+    };
+
+    var chart = new Chart(
+      chartElement,
+      {
+        "type": 'doughnut',
+        "data": data,
+        "options": {
+          "cutoutPercentage": 50,
+        }
+      }
+    );
+
+
+  }
 
 }
