d6cb4e668a2d792355f7a478099f2ff2a1370f12
atheodosiou <anastasios.theodosiou@gmail.com>
2018-05-26 11:49:14 +0300

    changes

--- src/app/components/maps/maps.component.ts
+++ src/app/components/maps/maps.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ViewChild } from '@angular/core';
+import { } from '@types/googlemaps';
 
 @Component({
   selector: 'app-maps',
@@ -7,9 +9,87 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapsComponent implements OnInit {
 
+  @ViewChild('gmap') gmapElement: any;
+  map: google.maps.Map;
+
+
+  latitude: any;
+  longitude: any;
+
+  iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
+
+  markerTypes = [
+    {
+      text: "Parking", value: "parking_lot_maps.png"
+    }
+    // ,
+    // {
+    //   text: "Library", value: "library_maps.png"
+    // },
+    // {
+    //   text: "Information", value: "info-i_maps.png"
+    // }
+  ];
+
+  selectedMarkerType: string = "parking_lot_maps.png";
+
   constructor() { }
 
   ngOnInit() {
+    var mapProp = {
+      center: new google.maps.LatLng(18.5793, 73.8143),
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
   }
 
+  setMapType(mapTypeId: string) {
+    this.map.setMapTypeId(mapTypeId)
+  }
+
+  setCenter() {
+    this.map.setCenter(new google.maps.LatLng(this.latitude, this.longitude));
+
+    let location = new google.maps.LatLng(this.latitude, this.longitude);
+
+    let marker = new google.maps.Marker({
+      position: location,
+      map: this.map,
+      title: 'Got you!'
+    });
+
+    marker.addListener('click', this.simpleMarkerHandler);
+
+    marker.addListener('click', () => {
+      this.markerHandler(marker);
+    });
+  }
+
+  simpleMarkerHandler() {
+    alert('Simple Component\'s function...');
+  }
+
+  markerHandler(marker: google.maps.Marker) {
+    alert('Marker\'s Title: ' + marker.getTitle());
+  }
+
+  showCustomMarker() {
+
+
+    this.map.setCenter(new google.maps.LatLng(this.latitude, this.longitude));
+
+    let location = new google.maps.LatLng(this.latitude, this.longitude);
+
+    console.log(`selected marker: ${this.selectedMarkerType}`);
+    
+    let marker = new google.maps.Marker({
+      position: location,
+      map: this.map,
+      icon: this.iconBase + this.selectedMarkerType,
+      title: 'Got you!'
+    });
+
+  }
+  
 }
--- src/app/components/maps/maps.module.ts
+++ src/app/components/maps/maps.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MapsComponent } from './maps.component';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms'
 
 @NgModule({
   imports: [
-    CommonModule,
+    CommonModule,FormsModule,
     RouterModule.forChild([{path: '', component: MapsComponent}])
   ],
   declarations: [MapsComponent]
