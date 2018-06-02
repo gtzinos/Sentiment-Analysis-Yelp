22d3d81076c85f782ee284da93bd683b2e4e4ad4
George Tzinos <geotzinos@gmail.com>
2018-06-02 17:28:03 +0300

    FIX Connect

--- backend/dbQueries/connect.py
+++ backend/dbQueries/connect.py
@@ -1,7 +1,10 @@
+# -*- coding: utf-8 -*-
 import pymongo
 
-def openConnection(hostname, dbname, port):
-    connection = pymongo.MongoClient(hostname, port)
+def openConnection(hostname, dbname, port, username, password, authMechanism):
+    connection = pymongo.MongoClient(hostname, port, username=username, password=password,
+                                     authSource=dbname, authMechanism=authMechanism)
+    
     return connection[dbname]
 
 def closeConnection(connection):
