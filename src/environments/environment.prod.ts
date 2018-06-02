214e26b33a7b44c045f9fb9097070e9ebeb801af
George Tzinos <geotzinos@gmail.com>
2018-06-02 17:39:54 +0300

    FIX: Prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  api: "yelp.geotzinos.com:5000",
-  mlapi: "yelp.geotzinos.com:5001"
+  api: "http://yelp.geotzinos.com:5000",
+  mlapi: "http://yelp.geotzinos.com:5001"
 };
